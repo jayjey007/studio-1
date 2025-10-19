c9bcc47ec29e1ac1a8e906e9aeb0c4f83e2a83ce
Jay j <j.jayachandran@gmail.com>
2025-10-19 17:04:30 +0000

    Error: Invalid src prop (https://i.pravatar.cc/40?u=user1) on `next/imag

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/**',
       }
     ],
   },

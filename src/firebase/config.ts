51fd84e62609837d4a20dd8a1b9ef2330553819a
Jay j <j.jayachandran@gmail.com>
2025-10-20 23:14:53 +0000

    image fix

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9367397757-f04cc",
   "appId": "1:129385794267:web:bac743908597ea0c44dafe",
-  "storageBucket": "studio-9367397757-f04cc.appspot.com",
+  "storageBucket": "studio-9367397757-f04cc.firebasestorage.app",
   "apiKey": "AIzaSyBqUs7aTQniRqK3LMBD0IJZWxhJbZCsoik",
   "authDomain": "studio-9367397757-f04cc.firebaseapp.com",
   "measurementId": "",

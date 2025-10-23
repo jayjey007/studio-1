915b16509b74d823dc25c3da16a619037683a37b
Jay j <j.jayachandran@gmail.com>
2025-10-23 21:01:30 +0000

    on blur logout is not working as expected

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -183,7 +183,7 @@ export default function ChatPage() {
       if (isFilePickerOpen.current) {
         return;
       }
-      // handleLogout();
+      handleLogout();
     };
 
     const handleWindowFocus = () => {
